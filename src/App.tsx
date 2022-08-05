ab938f6dec4e30bea3eff32f4d545ac081f14487
fleck-sara <sfleck@udel.edu>
2022-08-05 17:02:37 -0400

    added basic info & pic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import "./App.css";
+import pic from "./images/profile pic.jpg";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">Sara Fleck</header>
-        </div>
+        <>
+            <div className="App">
+                <div>
+                    <header className="App-header">Sara Fleck</header>
+                </div>
+                <p></p>
+                <div>
+                    <img className="Profile" src={pic} />
+                </div>
+                <div className="About"> About Me</div>
+                <ul className="Body">
+                    <li>
+                        Undergraduate Exercise Science & Computer Science
+                        student at the University of Delaware
+                    </li>
+                    <li>
+                        {" "}
+                        Research assistant with the Falls and Mobility Lab{" "}
+                    </li>
+                    <li>
+                        Teaching assistant for introductory computer science
+                        classes at UD
+                    </li>
+                    <li>Towamencin swim team coach</li>
+                </ul>
+            </div>
+        </>
     );
 }
 
